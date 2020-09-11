fd3257211a9a71fc609d7ebdde3dbd29a29b7f4f
zmw <1432140810@qq.com>
2020-09-11 15:37:44 +0800

    create base model

--- src/main/java/com/training/manager/model/Category.java
+++ src/main/java/com/training/manager/model/Category.java
@@ -1,4 +1,9 @@
 package com.training.manager.model;
 
+import java.math.BigDecimal;
+
 public class Category {
+    private Integer id;
+    private String name;
+    private BigDecimal budget;
 }
--- src/main/java/com/training/manager/model/Transaction.java
+++ src/main/java/com/training/manager/model/Transaction.java
@@ -1,4 +1,13 @@
 package com.training.manager.model;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Transaction {
+    private Integer id;
+    private String name;
+    private Category category;
+    private Date transactTime;
+    private BigDecimal amount;
+    private String detail;
 }
