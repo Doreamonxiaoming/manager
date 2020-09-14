8c0323d57b5ccd0f8852eb2cc9882d262e74ad42
zmw <1432140810@qq.com>
2020-09-14 10:18:44 +0800

    add some comment on test

--- src/test/java/com/training/manager/dao/DaoTest.java
+++ src/test/java/com/training/manager/dao/DaoTest.java
@@ -23,11 +23,11 @@ public class DaoTest {
         Category categoryCreate = new Category("create", new BigDecimal(400));
         Category categoryUpdate = new Category(3,"update", new BigDecimal(300));
 
-        System.out.println(categoryRepository.findById(1).get());;//通过id查询
+        categoryRepository.findById(1).get();//通过id查询
         categoryRepository.findAll();//查询所有
         categoryRepository.save(categoryCreate);//save没有id的对象是创建记录
         categoryRepository.save(categoryUpdate);//save有id的对象是更新记录
-        categoryRepository.deleteById(2);//根据id删除记录
+        categoryRepository.deleteById(2);//根据id删除记录//如果没有对应id会报错哦
         System.out.println(categoryRepository.count());//查询所有记录条数
         System.out.println(categoryRepository.existsById(3));//判断id对应的记录是否存在
     }
