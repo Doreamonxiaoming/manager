78b70cbd139a908720f7e23ecec04a20a7b38b56
katesxiao <shanxiao@andrew.cmu.edu>
2020-09-14 17:38:21 +0800

    add test file -TODO

---
+++ src/test/java/com/training/manager/controller/CategoryControllerTest.java
@@ -0,0 +1,33 @@
+package com.training.manager.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getAllCategory() {
+    }
+
+    @Test
+    public void getOneCategory() {
+    }
+
+    @Test
+    public void addCategory() {
+    }
+
+    @Test
+    public void modifyCategory() {
+    }
+
+    @Test
+    public void deleteCategory() {
+    }
+}
