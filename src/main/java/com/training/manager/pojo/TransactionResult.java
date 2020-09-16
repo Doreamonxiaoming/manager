aa2b92477bc264f0051334b6e5357f84ba8a6290
zmw <1432140810@qq.com>
2020-09-16 11:05:53 +0800

    add a unit test

--- src/main/java/com/training/manager/pojo/TransactionResult.java
+++ src/main/java/com/training/manager/pojo/TransactionResult.java
@@ -1,9 +1,6 @@
 package com.training.manager.pojo;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,14 +9,11 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class TransactionResult {
 
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
--- src/test/java/com/training/manager/service/TransactionServiceImplTest.java
+++ src/test/java/com/training/manager/service/TransactionServiceImplTest.java
@@ -1,23 +1,41 @@
 package com.training.manager.service;
 
+import com.training.manager.dao.TransactionRepository;
+import com.training.manager.model.Category;
 import com.training.manager.model.Transaction;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Optional;
 
 import static org.junit.Assert.*;
 
 public class TransactionServiceImplTest {
-    @Autowired
-    TransactionService transactionService;
+    @InjectMocks
+    TransactionServiceImpl transactionService;
+    @Mock
+    TransactionRepository transactionRepository;
 
+    Transaction transaction;
+    Category category;
     @Before
-    public void setUp() throws Exception {
-    }
+    public void setUp() {
+        //在单元测试之前，执行Mockito的注解扫描，并注入依赖
+        MockitoAnnotations.initMocks(this);
 
-    @After
-    public void tearDown() throws Exception {
+        // mock database result
+        transaction = new Transaction().builder().id(1).name("test").amount(new BigDecimal(100)).detail("test").build();
+        category = new Category().builder().id(1).name("test").budget(new BigDecimal(100)).transactions(new HashSet<>()).build();
+        transaction.setCategory(category);
+        category.getTransactions().add(transaction);
     }
 
     @Test
@@ -27,6 +45,9 @@ public class TransactionServiceImplTest {
 
     @Test
     public void getOneTransaction() {
+        Mockito.when(transactionRepository.findById(1)).thenReturn(Optional.of(transaction));
+
+        assertEquals(1,transactionService.getOneTransaction(1).getId().intValue());
     }
 
 //    @Test
